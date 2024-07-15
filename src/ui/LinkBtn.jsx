23e381c7eb0706697ba70dc43cc3cda0ede477a0
nabilmersni <nabilmersni123@gmail.com>
2024-07-15 15:24:11 +0100

    Implement custom LinkBtn component for the NavBar

--- src/ui/LinkBtn.jsx
+++ src/ui/LinkBtn.jsx
@@ -1,11 +1,46 @@
+import Lottie from 'lottie-react';
+
 import { NavLink } from 'react-router-dom';
+import campFire from '../assets/lottiesAnimations/camp-firee.json';
+import { useRef } from 'react';
 
 function LinkBtn({ to, children }) {
+  const lottieRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (lottieRef.current) {
+      lottieRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (lottieRef.current) {
+      lottieRef.current.stop();
+    }
+  };
+
   return (
-    <li>
-      <NavLink to={to} className="px-4 py-2">
-        {children}
+    <li
+      className="relative"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <NavLink
+        to={to}
+        className="navLink relative z-20 w-16 rounded-full text-lg font-bold text-slate-300 transition-all hover:text-[#fefdff]"
+      >
+        <span className="flex min-w-16 items-center justify-center">
+          {children}
+        </span>
       </NavLink>
+
+      <Lottie
+        className="navAnimation absolute bottom-0 left-0 right-0 top-0 z-10 m-auto h-16 w-16 origin-bottom opacity-0"
+        animationData={campFire}
+        loop={true}
+        autoPlay={false}
+        lottieRef={lottieRef}
+      />
     </li>
   );
 }
--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -1,18 +1,58 @@
 import { NavLink } from 'react-router-dom';
 import LinkBtn from './LinkBtn';
+import campFire3 from '../assets/lottiesAnimations/camp-fire-2.json';
+import Lottie from 'lottie-react';
 
 function NavBar() {
   return (
-    <nav className="bg-primary-dark">
-      <div className="flex items-center justify-between px-10 py-4">
+    <nav className="relative bg-primary-dark">
+      <div className="mx-auto flex max-w-[86rem] items-center justify-between px-10 py-4">
         <NavLink to={'/'}>
           <img className="w-12 lg:w-14" src="img/navLogo.svg" alt="" />
         </NavLink>
 
-        <ul>
-          <LinkBtn to={'/'}>hello</LinkBtn>
+        <ul className="flex items-center gap-6">
+          <LinkBtn to={'/'}>Home</LinkBtn>
+          <Lottie
+            className="h-10 w-10 opacity-50"
+            animationData={campFire3}
+            loop={true}
+          />
+          <LinkBtn to={'/'}>Our Values</LinkBtn>
+          <Lottie
+            className="h-10 w-10 opacity-50"
+            animationData={campFire3}
+            loop={true}
+          />
+          <LinkBtn to={'/'}>Events</LinkBtn>
+          <Lottie
+            className="h-10 w-10 opacity-50"
+            animationData={campFire3}
+            loop={true}
+          />
+          <LinkBtn to={'/'}>Shop</LinkBtn>
         </ul>
       </div>
+
+      {/* <div className="absolute bottom-0 left-96 right-0 top-0 z-10 m-auto flex items-center gap-10">
+        <Lottie
+          className="h-16 w-16 opacity-5"
+          animationData={campFire2}
+          loop={true}
+        />
+
+        <Lottie
+          className="h-16 w-16 opacity-5"
+          animationData={campFire3}
+          loop={true}
+        />
+
+        <Lottie
+          className="h-16 w-16 opacity-50"
+          animationData={campFire4}
+          loop={true}
+        />
+      </div> */}
     </nav>
   );
 }
