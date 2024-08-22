1733d792f30ae704ebcb5767064552d34dc5b076
nabilmersni <nabilmersni123@gmail.com>
2024-08-22 21:11:56 +0100

    Fix responsive issue in custom image carousel

--- src/ui/GaloryCarousel.jsx
+++ src/ui/GaloryCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -13,6 +13,35 @@ import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 function GaloryCarousel() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  const [slidesPerView, setSlidesPerView] = useState(4);
+
+  useEffect(() => {
+    const updateSlidesPerView = () => {
+      const width = window.innerWidth;
+      if (width > 1300) {
+        setSlidesPerView(7);
+      } else if (width > 1200) {
+        setSlidesPerView(6);
+      } else if (width > 900) {
+        setSlidesPerView(5);
+      } else if (width > 600) {
+        setSlidesPerView(4);
+      } else {
+        setSlidesPerView(2);
+      }
+    };
+    // Initial update
+    updateSlidesPerView();
+
+    // Update on window resize
+    window.addEventListener('resize', updateSlidesPerView);
+
+    // Cleanup the event listener
+    return () => {
+      window.removeEventListener('resize', updateSlidesPerView);
+    };
+  }, []);
 
   return (
     <div className="flex h-fit w-full flex-col overflow-hidden">
@@ -148,7 +177,7 @@ function GaloryCarousel() {
       <Swiper
         onSwiper={setThumbsSwiper}
         spaceBetween={20}
-        slidesPerView={7}
+        slidesPerView={slidesPerView}
         freeMode={true}
         watchSlidesProgress={true}
         modules={[FreeMode, Navigation, Thumbs]}
